74d5a7afafd3400cba39ad8b584c7b5a3cd28ebe
Magnus <magnuskollip@hotmail.dk>
2017-05-03 21:10:08 +0200

    Revert "Revert "updated to match the work of the  mazzopazzo""
    This reverts commit 7bd211456de8c08244f3e032b46e40286099aee7.

--- src/java/lolsoap/LolSOAPI.java
+++ src/java/lolsoap/LolSOAPI.java
@@ -23,19 +23,15 @@ public interface LolSOAPI {
 		@WebMethod UUID[] findGames();
 		@WebMethod String[] getUsernames(UUID gameId);
 		@WebMethod UUID[] getPlayersGames(String username);
-
 		@WebMethod void joinGame(UUID gameId, String username);
 		@WebMethod void startGame(String username);
-        @WebMethod boolean isGameStarted(String username);
+                @WebMethod boolean isGameStarted(String username);
 		@WebMethod boolean isGameDone(String username);
 		@WebMethod boolean didIWin(UUID gameId, String username);
-        @WebMethod String getWinner(UUID gameId);
-        
+                @WebMethod String getWinner(UUID gameId);
 		@WebMethod String getChampionImgUrl(String username);
 		@WebMethod String getChampionTitle(String username);    
-		
 		@WebMethod boolean guessChampion(String username, String guess);
-		
 		@WebMethod void skipChampion(String username);
-        @WebMethod String hentBruger(String user, String pass);
+                @WebMethod String hentBruger(String user, String pass);
 }
